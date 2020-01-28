52e1fd7b5680c8f79499387ea323058abb693703
Carlos Perez <carlosperezmolano@gmail.com>
2020-01-28 22:12:51 +0100

    First commit
    A sin to have this folder without version control.

---
+++ config.el
@@ -0,0 +1,74 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; refresh' after modifying this file!
+
+
+;; These are used for a number of things, particularly for GPG configuration,
+;; some email clients, file templates and snippets.
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "monospace" :size 14)
+      doom-variable-pitch-font (font-spec :family "ETBembo" :size 15))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+;; (setq doom-theme 'doom-one)
+(setq doom-theme 'doom-solarized-light)
+
+;; If you intend to use org, it is recommended you change this!
+(setq org-directory "~/my_files/")
+
+(after! org
+  (setq +org-capture-todo-file "projects/breathe.org")
+  (setq +org-capture-projects-file "projects/projects.org")
+  (setq org-agenda-files '("~/my_files/projects/breathe.org"))
+  )
+
+(after! org-journal
+  (defun org-journal-save-entry-and-exit()
+    "Simple convenience function.
+  Saves the buffer of the current day's entry and kills the window
+  Similar to org-capture like behavior"
+    (interactive)
+    (save-buffer)
+    (kill-buffer-and-window))
+  (define-key org-journal-mode-map (kbd "C-x C-s") 'org-journal-save-entry-and-exit)
+  )
+
+(setenv "WORKON_HOME" "/home/cperezm/miniconda3/envs")
+
+;; If you want to change the style of line numbers, change this to `relative' or
+;; `nil' to disable it:
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+;; Grammar check
+(setq langtool-language-tool-jar "~/LanguageTool-4.8/languagetool-commandline.jar" )
